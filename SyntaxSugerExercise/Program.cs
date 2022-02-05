3b72d9e04fbf5ea611310f42b1fa0456201efa32
macuser301 <macuser30@yahoo.com>
2022-02-04 21:03:03 -0600

    first commit

---
+++ SyntaxSugerExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SyntaxSugerExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int answer = 88.9;
+              string response;
+
+              if (answer < 47)
+              {
+                  response = answer + " is less than forty-seven";
+              }
+              else
+              {
+                  response = answer + “greater than or equal to forty-seven”;
+              }*/
+
+            var answer = 88.9;
+            var response = (answer < 47) ? $"{answer} is less than forty-seven" : $"{answer} is greater than forty-seven";
+
+            Console.WriteLine(response);
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
